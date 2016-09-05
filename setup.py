e104d3787085995bde8906252c81255e8b9cad0d
Joshua Spence <josh@freelancer.com>
2016-09-05 11:08:39 +1000

    Add a `setup.py` file
    Add a `setup.py` file and reorganize the code into a `vtctrans` module to allow installation via `pip`.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='vtctrans',
+    version='0.0-alpha2-varnish30',
+    description='Re-format tool for varnishtest',
+    packages=['vtctrans'],
+    install_requires=[
+        'simplejson',
+        'gevent >= 1.0.2, <2.0',
+        'boto >=2.0.0, <3.0.0',
+        'pyyaml >=3.10, <4.0'
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'vtctrans = vtctrans:main',
+        ],
+    },
+)
